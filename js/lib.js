60b4c313eb75e8f6f509a26da787223ff704d21f
senj1212 <senj1212123@gmail.com>
2020-06-07 16:09:01 +0300

    12

--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,48 @@
 $('ducument').ready(function(){
-    loadGoods();
+    loadPage();
 });
 
-function loadGoods(){
-    $.getJSON('goods.json', function (data){
-        console.log(data);
+function loadPage(){
+    $('#PyLink').click(function(){
+        loadPy();
+    });
+    $('#CLink').click(function(){
+        loadC();
+    });
+    $('#LuaLink').click(function(){
+        alert("Данный раздел временно пуст");
+    });
+    $('#JSLink').click(function(){
+        alert("Данный раздел временно пуст");
+    });
+}
+
+function loadPy(){
+    $.getJSON('projectPy.json', function (data){
+        console.log("1");
+        var out = '';
+        for (var key in data){
+            out += '<div class="project">';
+            out += '<img src="'+data[key].img+'">';
+            out += '<p>'+data[key].name+'</p>';
+            out += '<p>'+data[key].desc+'</p>';
+            out += '</div>';
+        }
+        $('#tables').html(out);
+    })
+}
+
+function loadC(){
+    console.log("2");
+    $.getJSON('projectC.json', function (data){
+        var out = '';
+        for (var key in data){
+            out += '<div class="project">';
+            out += '<img src="'+data[key].img+'">';
+            out += '<p>'+data[key].name+'</p>';
+            out += '<p>'+data[key].desc+'</p>';
+            out += '</div>';
+        }
+        $('#tables').html(out);
     })
 }
