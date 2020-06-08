bd959d1e35020aeee4c5ded577ffde18e95fa278
senj1212 <senj1212123@gmail.com>
2020-06-08 19:45:02 +0300

    1

--- js/lib.js
+++ js/lib.js
@@ -2,32 +2,32 @@ $('ducument').ready(function(){
     loadPage();
 });
 
-function loadRemoute(){
-    console.log('test1');
-    $.getJSON('remoute.json', function(data){
-        console.log(data);
-        var out = '';
-        for (var key in data){
-            console.log(data);
-            out += '<div class="day">';
-            out += '<p class="day_name">'+ key +'<p>';
-            out += '</div>';
-        }
-        $('.days').html(out);
+function loadPage(){
+    $('#PyLink').click(function(){
+        loadProjects('projectPy.json');
+    });
+    $('#CLink').click(function(){
+        loadProjects('projectC.json');
+    });
+    $('#LuaLink').click(function(){
+        alert("Данный раздел временно пуст");
+    });
+    $('#JSLink').click(function(){
+        alert("Данный раздел временно пуст");
     });
 }
 
-function loadPage(){
-    $('#diary').click(function(){
-        alert("Данный раздел временно пуст");
-    });
-    $('#education').click(function(){
-        alert("Данный раздел временно пуст");
-    });
-    $('#progress').click(function(){
-        alert("Данный раздел временно пуст");
-    });
-    $('#remoute').click(function(){
-        loadRemoute();
-    });
+function loadProjects(nameJSON){
+    $.getJSON(nameJSON, function (data){
+        var out = '';
+        for (var key in data){
+            out += '<div class="project">';
+            out += '<img class="img_table" src="'+data[key].img+'" width="230" height="160">';
+            out += '<p class="line_img"></p>'
+            out += '<p class="nameProject">'+data[key].name+'</p>';
+            out += '<p class="descProject">'+data[key].desc+'</p>';
+            out += '</div>';
+        }
+        $('.table').html(out);
+    })
 }
